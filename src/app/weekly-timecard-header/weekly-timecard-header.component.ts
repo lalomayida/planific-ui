64ce7b05d44ba8107f175397c730cd67ff740c73
Luis Eduardo Mayida González <eduardomayida@MacBook-Pro-de-Luis.local>
2021-02-17 09:23:30 -0600

    Adding range senlection, missing to enable functionality

--- src/app/weekly-timecard-header/weekly-timecard-header.component.ts
+++ src/app/weekly-timecard-header/weekly-timecard-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCalendar, NgbDate, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-weekly-timecard-header',
@@ -14,12 +14,45 @@ export class WeeklyTimecardHeaderComponent implements OnInit {
   @Output() onEditTitle: EventEmitter<any> = new EventEmitter();
   @Output() onEditDate: EventEmitter<any> = new EventEmitter();
 
-  today: NgbDateStruct;
+  hoveredDate: NgbDate | null = null;
 
-  constructor(private calendar: NgbCalendar) {}
+  fromDate: NgbDate | null;
+  toDate: NgbDate | null;
+
+  constructor(private calendar: NgbCalendar, public formatter: NgbDateParserFormatter) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+  }
+
+  onDateSelection(date: NgbDate) {
+    if (!this.fromDate && !this.toDate) {
+      this.fromDate = date;
+    } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+      this.toDate = date;
+    } else {
+      this.toDate = null;
+      this.fromDate = date;
+    }
+  }
+
+  isHovered(date: NgbDate) {
+    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+  }
+
+  isInside(date: NgbDate) {
+    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+  }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+  }
+
+  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+    const parsed = this.formatter.parse(input);
+    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+  }
 
   ngOnInit(): void {
-    this.today = this.calendar.getToday();
   }
 
   editTitle(){
