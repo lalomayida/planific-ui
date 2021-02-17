7d70904b47ebce250b42ca5ab351e69e67b0d7f6
Luis Eduardo Mayida González <eduardomayida@MacBook-Pro-de-Luis.local>
2021-02-17 11:35:55 -0600

    Implementación del límite de 5 días

--- src/app/weekly-timecard-header/weekly-timecard-header.component.ts
+++ src/app/weekly-timecard-header/weekly-timecard-header.component.ts
@@ -1,34 +1,45 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgbCalendar, NgbDate, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { isBuffer } from 'util';
 
 @Component({
   selector: 'app-weekly-timecard-header',
   templateUrl: './weekly-timecard-header.component.html',
   styleUrls: ['./weekly-timecard-header.component.css']
 })
+
 export class WeeklyTimecardHeaderComponent implements OnInit {
 
   @Input() title: string;
   @Input() date: NgbDateStruct;
-  
+
   @Output() onEditTitle: EventEmitter<any> = new EventEmitter();
   @Output() onEditDate: EventEmitter<any> = new EventEmitter();
 
   hoveredDate: NgbDate | null = null;
 
+  today: NgbDate | null;
+  upperLimitDate: NgbDate | null;
   fromDate: NgbDate | null;
   toDate: NgbDate | null;
 
   constructor(private calendar: NgbCalendar, public formatter: NgbDateParserFormatter) {
+    this.today = calendar.getToday();
     this.fromDate = calendar.getToday();
-    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 5);
   }
 
   onDateSelection(date: NgbDate) {
+
     if (!this.fromDate && !this.toDate) {
       this.fromDate = date;
     } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
       this.toDate = date;
+      this.upperLimitDate = this.calendar.getNext(this.fromDate, 'd', 5);
+      if (new Date(this.toDate.year, this.toDate.month - 1, this.toDate.day) > new Date(this.upperLimitDate.year, this.upperLimitDate.month - 1, this.upperLimitDate.day)) {
+        console.log("Selecciona un rango de fechas menor a 5 días");
+        this.toDate = null;
+      }
     } else {
       this.toDate = null;
       this.fromDate = date;
@@ -55,11 +66,11 @@ export class WeeklyTimecardHeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  editTitle(){
+  editTitle() {
     //this.onEditTitle.emit(this.title);
   }
 
-  editDate(){
+  editDate() {
     //this.onEditDate.emit(this.date);
   }
 
